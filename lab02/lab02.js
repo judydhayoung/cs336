836592116e21cd7cd229de8e5875fe734ceb42a6
Judy Kwon <33335774+judydhayoung@users.noreply.github.com>
2018-09-12 14:50:43 -0400

    add more testing code

--- lab02/lab02.js
+++ lab02/lab02.js
@@ -20,9 +20,9 @@ Person.prototype.changeName = function(newName) {
 
 //accessor method that computes age; cf.a pretty decent function for this from Naveen Jose.)
 //function copied from http://jsfiddle.net/codeandcloud/n33RJ/
-Person.prototype.getAge = function(dateString) {
+Person.prototype.getAge = function() {
     var today = new Date();
-    var birthDate = new Date(dateString);
+    var birthDate = new Date(this.birthdate);
     var age = today.getFullYear() - birthDate.getFullYear();
     var m = today.getMonth() - birthDate.getMonth();
     if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
@@ -36,7 +36,7 @@ Person.prototype.addFriend = function(friend) {
     this.friendList.push(friend);
     }
 
-//method to print a greeting(e.g., “I’m a person”)
+//method to print a greeting(e.g., "I'm a person")
 Person.prototype.greet = function greet() {
     console.log("Hello, I am " + this.name);
     }
@@ -44,10 +44,23 @@ Person.prototype.greet = function greet() {
 
 
 //Add some code to test your new prototype, e.g., create some instances, add friends, compare ages.
-var person1 = new Person('Donald', 'Jan-01-2000', []);
+var person1 = new Person('Donald', '2000/03/5', []);
 console.log(person1);
 person1.changeName('Mark');
 person1.addFriend('Kevin');
 person1.addFriend('Greg');
-person1.getAge(this.birthdate);
-console.log('age: ' + getAge("1980/08/10"));
+console.log(person1.name + " now has friend(s): " + person1.friendList);
+console.log(person1.name + "'s age: " + person1.getAge());
+
+var person2 = new Person('Kevin', '1985/03/5', []);
+console.log(person2);
+person2.addFriend('Mark');
+console.log(person2.name + " now has friend(s): " + person2.friendList);
+console.log(person2.name + "'s age: " + person2.getAge());
+if (person1.getAge() < person2.getAge()) {
+	console.log(person2.name + " is older than " + person1.name);
+} else if (person1.getAge() > person2.getAge()) {
+	console.log(person1.name + " is older than " + person2.name);
+} else {
+	console.log(person1.name + " and " + person2.name + " are of the same age");
+}
